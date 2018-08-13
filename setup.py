8fd05c5e0a8b9943149e38846d42749e2e4e0835
Julien Hillairet <julien.hillaire@cea.fr>
2018-08-13 19:44:58 +0200

    Added/moved the main launcher to the root of the project

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='ppat',
-      version='0.1',
-      description='Pre Post Analysis Tools for WEST',
+setup(name='pppat',
+      version='0.2',
+      description='Pre Post Pulse Analysis Tools for WEST',
       url='',
-      author='',
-      author_email='',
-      license='CECILL',
-      packages=['ppat'],
+      author='Julien Hillairet',
+      author_email='julien.hillairet@gmail.com',
+      license='GPL3',
+      packages=['pppat'],
       zip_safe=False)
