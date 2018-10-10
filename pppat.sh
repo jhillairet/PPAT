34e04dba418bfb341541417a900d2a18231751bc
EIC INGENIEUR 12/12/2999 externe <eic@nunki.intra.cea.fr>
2018-10-10 11:32:13 +0200

    Updated bash script to launch PPPAT from my Python distribution on eic linux account

--- pppat.sh
+++ pppat.sh
@@ -1,6 +1,7 @@
-#!/bin/sh -x
-source /etc/profile.d/modules.sh
-module load python/3.3
+#!/bin/bash
 
-cd /Home/eic/PPAT/
-python ppat.py PPATConfig_EiC.xml
+##!/bin/sh -x
+#source /etc/profile.d/modules.sh
+#module load python/3.3
+# Use my Anaconda Python distribution which is much more up to date than the one of IRFM...
+/Home/JH218595/anaconda3/bin/python /Home/eic/PPPAT/main.py
