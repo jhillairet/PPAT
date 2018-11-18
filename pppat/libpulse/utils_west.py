7b32c44e8a7c317fc015bc4f9a667c4afeda327b
Julien Hillairet <julien.hillaire@cea.fr>
2018-11-18 15:49:35 +0100

    Added pulse_datetime to get the date, start and end times of a WEST pulse.

--- pppat/libpulse/utils_west.py
+++ pppat/libpulse/utils_west.py
@@ -37,6 +37,45 @@ def last_pulse_nb():
     else:
         return -1
 
+def pulse_datetime(pulse_nb):
+    """
+    Return the date, start and end times of a WEST pulse
+    
+    Parameters
+    ----------
+     - pulse_nb: int
+         WEST pulse number
+    
+    Return
+    ------
+     - date: str
+         date in format 'DD/MM/YY'. 
+     - t_start: str
+         data start time in format 'hh:mm:ss'.
+     - t_stop: str
+         data end time in format 'hh:mm:ss'
+     - pulse_dt: Timestamp (from pandas.Timestamp)
+         WEST pulse datetime
+    
+    NB: to convert the pulse start time into cumuluated seconds from midnight:
+        # pulse date Timestamp for reference (at 00:00:00)
+        pulse_day = pd.to_datetime(pulse_dt.date())
+        # duration between reference and pulse start in s
+        t_start_s = (pulse_dt - pulse_day).total_seconds()
+    """
+    # Durée entre ORIGINE et finacquisition corrected from IGNITRON
+    t_fin_acq = pw.tsmat(pulse_nb, 'FINACQ|1')[0]
+   
+    # date du choc
+    date_apilote = pw.tsmat(pulse_nb, 'APILOTE;+VME_PIL;Date_Choc')
+    pulse_dt = pd.to_datetime(date_apilote)
+    # date conversion in 'dd/MM/yy'
+    pulse_date = pulse_dt.strftime('%d/%m/%y')
+    t_start = pulse_dt.strftime('%H:%M:%S')
+    t_stop = (pulse_dt + pd.Timedelta(t_fin_acq, unit='s')).strftime('%H:%M:%S')
+    
+    return pulse_date, t_start, t_stop, pulse_dt
+
 def temperature_from_time(signame, date=None, t_start='00:01:00', t_stop='23:59:00'):
     """
     return a temperature signal from DCALOR from continuous acquisition
@@ -87,22 +126,13 @@ def temperature_from_pulse(pulse_nb, signame):
          time array in second. t=0 correspond to pulse DCALOR ORIGIN
 
     """
-    # Durée entre ORIGINE et finacquisition corrected from IGNITRON
-    t_fin_acq = pw.tsmat(pulse_nb, 'FINACQ|1')[0]
-
     # Récupération de la datation de DCALOR (en ms depuis minuit)
     # lorsqu'il recoit ORIGINE (commence l'acquisition)
     # Instant qui définit t=0s
     t_origine = int(pw.tsmat(pulse_nb, 'DCALOR;PARAM;ORIGINE'))
 
-    # date du choc
-    date_apilote = pw.tsmat(pulse_nb, 'APILOTE;+VME_PIL;Date_Choc')
-    pulse_datetime = pd.to_datetime(date_apilote)
-    # date conversion in 'dd/MM/yy'
-    pulse_date = pulse_datetime.strftime('%d/%m/%y')
-    t_start = pulse_datetime.strftime('%H:%M:%S')
-    t_stop = (pulse_datetime + pd.Timedelta(t_fin_acq, unit='s')).strftime('%H:%M:%S')
-
+    # continous time acquisition data
+    pulse_date, t_start, t_stop, _ = pulse_datetime(pulse_nb)
     T, t = pw.tsbase(signame, pulse_date, t_start, t_stop)
 
     # convert time into pulse time in second
