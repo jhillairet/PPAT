7e1980d482b6d4c4b91cdc46149b4331c5dabfb2
Julien Hillairet <julien.hillaire@cea.fr>
2018-11-25 21:18:50 +0100

    - solved the bug related to DCALOR time base. Instead of looking for the temperature data at the time of the pulse, now it gets the data from -1hour before to +1hour after the pulse, and then filter the data to the pulse times. This is the very same technique used by the IR team...

--- pppat/libpulse/utils_west.py
+++ pppat/libpulse/utils_west.py
@@ -40,23 +40,23 @@ def last_pulse_nb():
 def pulse_datetime(pulse_nb):
     """
     Return the date, start and end times of a WEST pulse
-    
+
     Parameters
     ----------
      - pulse_nb: int
          WEST pulse number
-    
+
     Return
     ------
      - date: str
-         date in format 'DD/MM/YY'. 
+         date in format 'DD/MM/YY'.
      - t_start: str
          data start time in format 'hh:mm:ss'.
      - t_stop: str
          data end time in format 'hh:mm:ss'
      - pulse_dt: Timestamp (from pandas.Timestamp)
          WEST pulse datetime
-    
+
     NB: to convert the pulse start time into cumuluated seconds from midnight:
         # pulse date Timestamp for reference (at 00:00:00)
         pulse_day = pd.to_datetime(pulse_dt.date())
@@ -65,46 +65,16 @@ def pulse_datetime(pulse_nb):
     """
     # Durée entre ORIGINE et finacquisition corrected from IGNITRON
     t_fin_acq = pw.tsmat(pulse_nb, 'FINACQ|1')[0]
-   
-    # date du choc
+
+    # date du LANCEMENT du choc
     date_apilote = pw.tsmat(pulse_nb, 'APILOTE;+VME_PIL;Date_Choc')
     pulse_dt = pd.to_datetime(date_apilote)
     # date conversion in 'dd/MM/yy'
     pulse_date = pulse_dt.strftime('%d/%m/%y')
     t_start = pulse_dt.strftime('%H:%M:%S')
     t_stop = (pulse_dt + pd.Timedelta(t_fin_acq, unit='s')).strftime('%H:%M:%S')
-    
+
     return pulse_date, t_start, t_stop, pulse_dt
-
-def temperature_from_time(signame, date=None, t_start='00:01:00', t_stop='23:59:00'):
-    """
-    return a temperature signal from DCALOR from continuous acquisition
-
-    Parameters
-    ----------
-     - signame: str
-         signal name
-     - date: str
-         date in format 'DD/MM/YY'. Default value: date of the day
-     - t_start: str
-         data start time in format 'hh:mm:ss'. Default value: 00:01:00
-     - t_stop: str
-         data end time in format 'hh:mm:ss'. Default value: 23:59:00
-
-    Return
-    -------
-     - T: np.array
-         temperature array
-     - t: np.array
-         time vector in ms from the beginning of the day (tbc)
-
-    """
-    if not date:
-        date = datetime.now().strftime('%d/%m/%y')
-
-    T, t = pw.tsbase(signame, date, t_start, t_stop)
-
-    return T, t
 
 
 def temperature_from_pulse(pulse_nb, signame):
@@ -126,15 +96,82 @@ def temperature_from_pulse(pulse_nb, signame):
          time array in second. t=0 correspond to pulse DCALOR ORIGIN
 
     """
+    pulse_date, t_start, t_stop, pulse_dt = pulse_datetime(pulse_nb)
     # Récupération de la datation de DCALOR (en ms depuis minuit)
     # lorsqu'il recoit ORIGINE (commence l'acquisition)
     # Instant qui définit t=0s
-    t_origine = int(pw.tsmat(pulse_nb, 'DCALOR;PARAM;ORIGINE'))
+    t_origine = int(pw.tsmat(pulse_nb, 'DCALOR;PARAM;ORIGINE'))/1e3  # in s
 
-    # continous time acquisition data
-    pulse_date, t_start, t_stop, _ = pulse_datetime(pulse_nb)
-    T, t = pw.tsbase(signame, pulse_date, t_start, t_stop)
+#    # Convertit t_origine en HH:MM:SS
+#    t_origine_str = (pd.to_datetime(pulse_date) + pd.Timedelta(t_origine, unit='s')).strftime('%H:%M:%S')
 
-    # convert time into pulse time in second
-    t = t - t_origine/1e3 - 32
+    # Durée entre ORIGINE et finacquisition corrected from IGNITRON
+    t_fin_acq = pw.tsmat(pulse_nb, 'FINACQ|1')[0]
+
+#    # Heure de la fin d'acquisition au format HH:MM:SS
+#    t_fin_acq_str = (pd.to_datetime(pulse_date) + pd.Timedelta(t_origine, unit='s') + pd.Timedelta(t_fin_acq, unit='s')).strftime('%H:%M:%S')
+
+    # La base de temps DCALOR est, disons, "perturbée". Un appel direct de
+    # tsbase utilisant l'heure de début et de fin de choc renvoie parfois
+    # des vecteurs temps incohérents. Pour palier cela, on télécharge une période
+    # de temps plus grande (ici +/- 1 heure)
+    # et on filtre ensuite les données relatives au choc.
+    t_before = (pulse_dt - pd.Timedelta(hours=1)).strftime('%H:%M:%S')
+    t_after = (pulse_dt + pd.Timedelta(hours=1)).strftime('%H:%M:%S')
+    T, t = pw.tsbase(signame, pulse_date, t_before, t_after)
+
+    idx = (t > t_origine) & (t < t_origine + t_fin_acq)
+    t2 = t[idx[:,0]] - t_origine -32 #  ms (absolute) -> s (relative)
+    T2 = T[idx[:,0]]
+
+    # keep only physical temperatures (>10°C)
+    t2 = np.where(T2 > 10, t2, np.NaN)
+    T2 = np.where(T2 > 10, T2, np.NaN)
+
+    return T2, t2
+
+
+def continuous_signal_from_time(signame, date=None, t_start='00:01:00', t_stop='23:59:00'):
+    """
+    return a continuous acquisition signal from two dates
+
+    Parameters
+    ----------
+     - signame: str
+         signal name
+     - date: str
+         date in format 'DD/MM/YY'. Default value: date of the day
+     - t_start: str
+         data start time in format 'hh:mm:ss'. Default value: 00:01:00
+     - t_stop: str
+         data end time in format 'hh:mm:ss'. Default value: 23:59:00
+
+    Return
+    -------
+     - T: np.array
+         signal
+     - t: np.array
+         time vector of the signal
+
+    """
+    if not date:
+        date = datetime.now().strftime('%d/%m/%y')
+
+    T, t = pw.tsbase(signame, date, t_start, t_stop)
+
     return T, t
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    pulse_nb = 53744
+    signame = 'GETC_ISP_6A'
+
+    T, t = temperature_from_pulse(pulse_nb, signame)
+
+    fig,ax = plt.subplots()
+    #ax.axvspan(0, t_fin_acq, alpha=0.2, color='red')
+    #ax.plot(t, T, '.')
+    ax.plot(t, T, '.')
+    #ax.set_xlim(- 5*60, t_fin_acq + 5*60)
+    fig.show()
+
--- tests/pre_pulse/WOI_6p1.py
+++ tests/pre_pulse/WOI_6p1.py
@@ -4,7 +4,7 @@ WOI 6.1 : lower divertor PFUs
 """
 from pppat.libpulse.check_result import CheckResult as Result
 from pppat.libpulse.waveform import get_waveform
-from pppat.libpulse.utils_west import temperature_from_time
+from pppat.libpulse.utils_west import continuous_signal_from_time
 import datetime
 import numpy as np
 import logging
@@ -14,13 +14,13 @@ logger = logging.getLogger(__name__)
 def check_WOI_6p1_initial_temperature(is_online=True, waveforms=None):
     """
     Check the max temperature on the lower divertor during the last 10 min.
-    
-    The temperature should be below 200°C in order to start a new pulse. 
+
+    The temperature should be below 200°C in order to start a new pulse.
     """
     CHECK_NAME = 'WOI 6.1: lower PFUs. Initial temperature.'
     T_ERROR = 200  # °C
     T_WARNING = 150  # °C
-    
+
     # get the current time and deduce the time 10 minutes before
     now = datetime.datetime.now()
     date = now.strftime('%d/%m/%y')
@@ -34,13 +34,15 @@ def check_WOI_6p1_initial_temperature(is_online=True, waveforms=None):
                         'GETC_RIPL_6A']  # TC toroidaux Q6A
     max_temp = []
     for sig in sig_TCs_divertor:
-        T, t = temperature_from_time(sig, date, 
-                                     t_start=time_ten_min_before, 
+        T, t = continuous_signal_from_time(sig, date,
+                                     t_start=time_ten_min_before,
                                      t_stop=time_now)
-        max_temp.append(np.amax(T))  # max of the signal 
+        # keep only realist temperatures (>10°C)
+        T2 = np.where(T > 10, T, -1)
+        max_temp.append(np.amax(T2))  # max of the signal
 
     max_max_temp = np.amax(max_temp)  # max of the max
-    
+
     if max_max_temp > T_ERROR:
         return Result(name=CHECK_NAME, code=Result.ERROR,
                       text=f'Max Temp Lower Divertor during last 10min: {max_max_temp:.0f}°C > {T_ERROR}')
@@ -49,28 +51,28 @@ def check_WOI_6p1_initial_temperature(is_online=True, waveforms=None):
                       text=f'Max Temp Lower Divertor during last 10min: {max_max_temp:.0f}°C > {T_WARNING}')
     else:  # I guess it is OK then
         return Result(name=CHECK_NAME, code=Result.OK,
-                      text=f'Max Temp Lower Divertor during last 10min: {max_max_temp:.0f}°C')   
+                      text=f'Max Temp Lower Divertor during last 10min: {max_max_temp:.0f}°C')
 
 
 #def check_WOI_6p1_Xpoint_duration(is_online=False, waveforms=None):
 #    """ Check the duration of the X-point phase """
 #    CHECK_NAME = 'WOI 6.1: lower PFUs'
 #    MAXIMUM_XPOINT_DURATION = 10  # s
-#    
+#
 #    waveform_name = 'rts:WEST_PCS/Actuators/Poloidal/IXb/waveform.ref'
 #    waveform = get_waveform(waveform_name, waveforms)
 #
 #    if not waveform:
 #        raise(ValueError(f'waveform {waveform_name} not found!?'))
-#    else:       
+#    else:
 #        IXb = waveform.values
 #        t_IXb = waveform.times
 #        nonzero_idx = np.argwhere(IXb != 0)
-#        
+#
 #        logger.info(np.array(np.where(IXb != 0)).squeeze() )
 #        logger.info(t_IXb[nonzero_idx].squeeze() )
-#        
-#        # TODO : the following fails. 
+#
+#        # TODO : the following fails.
 #        if len(nonzero_idx) > 0:
 #            #FirstNonZero_index = nonzero_idx[0]
 #            #LastZero_index = FirstNonZero_index - 1
@@ -80,13 +82,17 @@ def check_WOI_6p1_initial_temperature(is_online=True, waveforms=None):
 #            FinalZero_index = LastNonZero_index + 1
 #            TimeXpointStop = np.interp(0.0,IXb[LastNonZero_index:FinalZero_index],t_IXb[LastNonZero_index:FinalZero_index])
 #            duration_Xpoint = TimeXpointStop - TimeXpointStart
-#    
+#
 #        else:
 #            duration_Xpoint = 0.0
-#            
+#
 #        if duration_Xpoint > MAXIMUM_XPOINT_DURATION:
 #            return Result(name=CHECK_NAME, code=Result.ERROR,
 #                          text='Lower X-point phase too long: Lower divertor coils ON during')
 #        else:
 #            return Result(name=CHECK_NAME, code=Result.OK,
 #                          text='Lower X-point phase duration OK')
+
+
+if __name__ == '__main__':
+    res=check_WOI_6p1_initial_temperature()
