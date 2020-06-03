04cd139a9c4b5f91fb9dcf56f434f1d01d7ff372
Julien Hillairet <julien.hillairet@cea.fr>
2020-06-03 18:08:53 +0200

    switched Python version to default IRFM one

--- pppat.sh
+++ pppat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-##!/bin/sh -x
-#source /etc/profile.d/modules.sh
-#module load python/3.3
+# Use the default IRFM scientific Python distribution
+source /etc/profile.d/modules.sh
+module load tools_dc/4
+python ./main.py
 # Use my Anaconda Python distribution which is much more up to date than the one of IRFM...
-/Home/JH218595/anaconda3/bin/python /Home/eic/PPPAT/main.py
+#/Home/JH218595/anaconda3_36/bin/python ./main.py
