f180f68d50e0f33d2c9cf498e7576c26f6eba231
EiC <eic@talitha.intra.cea.fr>
2020-06-12 09:45:27 +0200

    allow launching PPPAT from any directory

--- pppat.sh
+++ pppat.sh
@@ -2,6 +2,7 @@
 # Use the default IRFM scientific Python distribution
 source /etc/profile.d/modules.sh
 module load tools_dc/4
-python ./main.py
+HERE=$(dirname ${BASH_SOURCE[0]})
+python $HERE/main.py
 # Use my Anaconda Python distribution which is much more up to date than the one of IRFM...
 #/Home/JH218595/anaconda3_36/bin/python ./main.py
