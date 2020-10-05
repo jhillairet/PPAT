46d4040f078a86544c6f3f4d58b130a582e38ce7
Julien Hillairet <julien.hillairet@cea.fr>
2020-10-05 14:35:43 +0200

    added files to launch control room independently of PPPAT

---
+++ run_control_room.py
@@ -0,0 +1,16 @@
+"""
+Control Room
+
+@authors: J.Hillairet
+"""
+# Running this script will launch control room's graphical user interface
+if __name__ == '__main__':
+    import sys
+    from pppat.ui.control_room.control_room import *
+
+    app = QApplication(sys.argv)
+    app.setStyle(QStyleFactory.create('Fusion'))
+    win = ControlRoom()
+    win.show()
+    sys.exit(app.exec_())
+    
